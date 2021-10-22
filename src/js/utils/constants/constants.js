ff485357b3c9dcf38a05fa9bd2e74e12d76eb68f
Gonzalo Gabriel Perez <zalitoar@gmail.com>
2021-10-22 20:04:14 -0300

    trying to fix it

--- src/js/utils/constants/constants.js
+++ src/js/utils/constants/constants.js
@@ -50,7 +50,8 @@ const PLUGINS = {
     "betterWMS": "./src/js/map/plugins/leaflet/leaflet-wms/leaflet.wms.js",
     "graticula": "./src/js/map/plugins/leaflet/leaflet-simplegraticule/L.SimpleGraticule.js",
     "WMTS": "./src/js/map/plugins/leaflet/leaflet-wmts/leaflet-tilelayer-wmts.js",
-    "EasyPrint": "./src/js/map/plugins/leaflet/leaflet-easyPrint/bundle.js"
+    "EasyPrint": "./src/js/map/plugins/leaflet/leaflet-easyPrint/bundle.js",
+    "TextPath": "./src/js/map/plugins/leaflet/leaflet-textpath/leaflet.textpath.js"
 };
 
 const MESSAGE_TIME = 4000;
